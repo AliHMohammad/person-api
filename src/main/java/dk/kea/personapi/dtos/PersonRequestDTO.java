49ce195199e846b0518376fbf40a963b6d41c2eb
AliHMohammad <alihmohammad@hotmail.com>
2024-04-29 21:07:23 +0200

    removed space

--- src/main/java/dk/kea/personapi/dtos/PersonRequestDTO.java
+++ src/main/java/dk/kea/personapi/dtos/PersonRequestDTO.java
@@ -3,7 +3,6 @@ package dk.kea.personapi.dtos;
 import jakarta.validation.constraints.NotNull;
 
 public record PersonRequestDTO(
-
         @NotNull(message = "firstName must be specified")
         String firstName,
         String middleName,
